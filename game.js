4957f69a4375a93c65b94bbb4bebd5e51a2d7baa
efhiii <efhiii2000@gmail.com>
2020-02-26 23:28:29 -0800

    some update

--- game.js
+++ game.js
@@ -35,6 +35,13 @@ function setup() {
   tile2 = createGraphics(400, 400);
   tile2.textSize(120);
 }
+var tileSize=0;
+function drawTile(x,y){
+  push();
+  translate(x,y);
+  plane(tileSize);
+  pop();
+}
 
 function draw() {
   background(0);
@@ -45,48 +52,60 @@ function draw() {
   tile2.background(0,200,0);
   tile2.text('Test', 100, 240);
 
-  var tileSize=min(width/4,height/4);
+  //noLights();
+  let dirX = (mouseX / width - 0.5) * 2;
+  let dirY = (mouseY / height - 0.5) * 2;
+
+  tileSize=min(width/10,height/10);
 
   //pass image as texture
-  texture(pg);
+  //texture(pg);
   push();
   translate(-width/2,-height/2);
+  translate(mouseX,mouseY);
   //translate(0,0,-500);
   //rotateX(mouseY/100);
   noStroke();
-  for(var i=0;i<4;i++){
-    push();
-    translate(mouseX,mouseY);
-    rotateZ(PI/2*i);
-    translate(0,tileSize/2);
-    rotateX(PI/2);
-    //plane(tileSize,tileSize/4);
-    image(pg,-tileSize/2,-tileSize/8,tileSize,tileSize/4);
-    pop();
+  fill(0);
+
+  for(var i=0;i<gameMaps[0].grid.length;i++){
+    for(var j=0;j<gameMaps[0].grid[i].length;j++){
+      switch(gameMaps[0].grid[i][j]){
+        case("#"):
+          fill(100);
+          push();
+          translate(j*tileSize,i*tileSize);
+          plane(tileSize);
+          fill(80);
+          for(var k=0;k<4;k++){
+            push();
+            rotateZ(PI/2*k);
+            translate(0,tileSize/2,-tileSize/8);
+            rotateX(PI/2);
+            plane(tileSize,tileSize/4);
+            pop();
+          }
+          pop();
+        break;
+        case("+"):
+          push();
+          translate(0,0,-tileSize/4);
+          fill(90);
+          drawTile(j*tileSize,i*tileSize);
+          pop();
+        break;
+      }
+    }
   }
-  translate(0,0,-tileSize/8);
-  image(tile,mouseX-tileSize/2,mouseY-tileSize/2,tileSize,tileSize);
-  translate(0,0,tileSize/4);
-  image(tile2,mouseX+tileSize/2,mouseY-tileSize/2,tileSize,tileSize);
-  image(tile2,mouseX-tileSize/2,mouseY+tileSize/2,tileSize,tileSize);
-  image(tile2,mouseX-tileSize/2,mouseY-tileSize*1.5,tileSize,tileSize);
-  image(tile2,mouseX-tileSize*1.5,mouseY-tileSize/2,tileSize,tileSize);
-
-  normalMaterial(); // For effect
-  push();
-    translate(mouseX,mouseY,0);
-    rotateX(PI/2);
-    scale(2);
-    //model(teapot);
 }
-
+/*
 function touchStarted () {
   var fs = fullscreen();
   if (!fs) {
     fullscreen(true);
   }
 }
-
+*/
 /* full screening will change the size of the canvas */
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
